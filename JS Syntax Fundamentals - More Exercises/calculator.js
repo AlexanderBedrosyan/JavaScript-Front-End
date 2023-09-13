1f25bc5b0cd764f4150d00f2f6c4fd9999206b1c
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2023-09-13 11:07:52 +0300

    Solutions for JS Syntax Fundamentals - More Exercises
    Solutions for JS Syntax Fundamentals - More Exercises

---
+++ JS Syntax Fundamentals - More Exercises/calculator.js
@@ -0,0 +1,16 @@
+function calculator(num1, operator, num2) {
+    switch(operator){
+        case '+':
+            console.log((num1 + num2).toFixed(2));
+            break;
+        case '-':
+            console.log((num1 - num2).toFixed(2));
+            break;
+        case '*':
+            console.log((num1 * num2).toFixed(2));
+            break;  
+        case '/':
+            console.log((num1 / num2).toFixed(2));
+            break;              
+    }
+}
