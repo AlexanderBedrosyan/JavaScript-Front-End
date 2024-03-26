9f879887bb181429bc51f3a5106a24e9bfcdc090
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-03-26 09:08:32 +0200

    Add Solutions for DOM Introductions - Lab
    Add Solutions for DOM Introductions - Lab

---
+++ DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let spanText = document.querySelector('span#text');
+    let linkReadMore = document.querySelector('#more');
+    linkReadMore.style.display = 'none'
+    spanText.style.display = 'inline';
+}
