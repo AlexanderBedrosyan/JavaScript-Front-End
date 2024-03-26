90a60958f69685beace4b89ec03fca748765b4c9
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-03-26 09:08:22 +0200

    Add Solutions for DOM Introductions - Lab
    Add Solutions for DOM Introductions - Lab

---
+++ DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let sum = document.getElementById('sum');
+    sum.value = Number(num1) + Number(num2)
+}
